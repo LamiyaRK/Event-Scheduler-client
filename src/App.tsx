74829373f6d0517d771600a4236de2cff4d65896
Lamiya <akabane755@gmail.com>
2025-07-20 01:56:37 +0600

    created Eventform ,EventDisplay files

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+const App: React.FC = () => {
+ 
+
+  
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+    
+    </div>
+  );
+};
+
+export default App;
