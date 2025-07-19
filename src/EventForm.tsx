e53f19313235bc4f1c73e4ac8058b77e47ce3fef
Lamiya <akabane755@gmail.com>
2025-07-20 02:48:14 +0600

    implemented handleSubmit

--- src/EventForm.tsx
+++ src/EventForm.tsx
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { useState } from "react";
+import { toast } from "react-toastify";
 
 
 interface EventFormProps {
@@ -29,8 +31,35 @@ const EventForm: React.FC<EventFormProps> = ({ onSubmit }) =>{
     [name]:value
     }))
   }
+ 
+  const handleSubmit=async(e:React.FormEvent)=>{
+    e.preventDefault();
+    if(!formData.title||!formData.time||!formData.date)
+    {
+      toast.error("'Please fill in all required fields(Title,time,date).'")
+      return;
+    }
+
+    try{
+       const response = await axios.post('http://localhost:5000/events', formData);
+      const newEvent = response.data;
+
+      toast.success('Event successfully saved! ✅');
+        onSubmit(newEvent); 
+          setFormData({ title: '', date: '', time: '', notes: '', category: 'Other' });
+
+
+
+    }catch(error)
+    {
+       console.error('Error saving event:', error);
+      toast.error('Something went wrong while saving the event ❌');
+
+    }
+  }
+
   return(
-    <form>
+    <form onClick={handleSubmit}>
          <h2 className="text-xl font-semibold text-center">Create New Event</h2>
          
       <input
@@ -93,3 +122,4 @@ const EventForm: React.FC<EventFormProps> = ({ onSubmit }) =>{
   );
 
 }
+export default EventForm;
